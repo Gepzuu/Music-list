f5f4c697f9484a56bab1750bf59a5bc47efedc51
Gepzu <johnmicgepulla@gmail.com>
2024-05-25 11:58:51 +0800

    update

---
+++ src/components/list-music.component.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
+
+export default class MusicList extends Component {
+
+    render(){
+        return (
+            <div className="container">Music List</div>
+        )
+    }
+
+}
